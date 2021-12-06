6bb581e9a3ec16e0f96c5e083ddf16c434f88c43
Wojciech Drewek <drewekwojciech@gmail.com>
2021-12-06 17:40:36 +0100

    minor improvements

--- graph_builder.R
+++ graph_builder.R
@@ -4,9 +4,11 @@ library(ggthemes)
 library(ggrepel)
 library(ggforce)
 
-setwd("C:/Users/oldst/Desktop/inz-skrypty/graph_builder/")
+setwd("C:/Users/oldst/Desktop/graph-builder/")
 source("constant.R")
 source("lib.R")
+
+inferences_per_model = 330
 
 process_model_data <- function(model) {
 	
@@ -24,11 +26,9 @@ process_model_data <- function(model) {
 	df <- data.frame()
 	for (i in seq(from=1, to=length(file_names), by=1)) {
 		csv_data = read.csv(file_names[[i]])
-		df_temp = process_data(file_names[[i]], csv_data, acc[[i]])
+		df_temp = process_data(file_names[[i]], csv_data, acc[[i]], inferences_per_model)
 		df <- rbind(df, df_temp)
 	}
-	#df_temp = remove_dominated(df_temp)
-	#df = fix_x(df)
 	return(df)
 }
 
@@ -47,36 +47,13 @@ build_all_models_graph <- function() {
 		#scale_shape_manual(values=c(21,17,23)) +  
 		#scale_fill_discrete(na.value=NA, guide="none") +
 		geom_text() + scale_color_manual(values=c("red", "yellow" , "#000000", "blue", "#80FFFF")) +
-		xlab("Srednie opóznienie [ms]") + ylab("Top1 Trafnosc [%]") + 
+		xlab("Srednie opoznienie [ms]") + ylab("Top1 Trafnosc [%]") + 
 		geom_text_repel(aes(x=df$latency, y=df$acc, label = df$model_labels),box.padding = 0.35, point.padding = 0.5,segment.color = 'grey50', size = 3) +
 		theme_bw() + theme(plot.title = element_text(size=22), axis.title = element_text(size=14), axis.text = element_text(size=13)) +
 		scale_size_manual(name = "precision", values=c(6,4))
 }
 
+build_all_models_graph()
 
-#build_all_models_graph()
 
-
-csv_data = read.csv("jn_results_mobilenet_v1_GPU.csv")
-df = process_data("jn_results_mobilenet_v1_GPU.csv", csv_data, acc_v1_top1, 20)
-
-csv_data = read.csv("jn_results_mobilenet_v2_GPU.csv")
-df_temp = process_data("jn_results_mobilenet_v2_GPU.csv", csv_data, acc_v2_top1, 30)
-df = rbind(df, df_temp)
-
-csv_data = read.csv("jn_results_mobilenet_v3_GPU.csv")
-df_temp = process_data("jn_results_mobilenet_v3_GPU.csv", csv_data, acc_v3_top1, 30)
-df = rbind(df, df_temp)
-
-df = remove_dominated(df)
-df = fix_x(df)
-
-ggplot() +
-	geom_point(data=df, aes(x=latency, y=acc, color=precision, shape=acceleration), size=4) +
-	#scale_shape_manual(values=c(21,17,23)) +  
-	#scale_fill_discrete(na.value=NA, guide="none") +
-	geom_text() + scale_color_manual(values=c("red", "yellow" , "#000000", "blue", "#80FFFF")) +
-	xlab("Srednie opóznienie [ms]") + ylab("Top1 Trafnosc [%]") + 
-	geom_text_repel(aes(x=df$latency, y=df$acc, label = df$model_labels),box.padding = 0.35, point.padding = 0.5,segment.color = 'grey50', size = 3) +
-	theme_bw() + theme(plot.title = element_text(size=22), axis.title = element_text(size=14), axis.text = element_text(size=13))
 
